fa8daf45e0c0b9e5ff764669207df11cab4a4560
Thomas <trescenzi@gmail.com>
2016-10-18 19:31:11 -0400

    add card text parsing

--- bot.js
+++ bot.js
@@ -1,10 +1,7 @@
 const discord = require('discord.js');
 const process = require('process');
 const scheduleGame = require('./src/scheduleGame');
-const fs = require('fs');
-const cards = JSON.parse(fs.readFileSync('cards-obj.json'));
-const cardNames = Object.keys(cards);
-const fuzzy = require('fuzzy');
+const {getCardText, cardSearch, getCardImage} = require('./src/paragon.js');
 const bot = new discord.Client();
 
 bot.match = function(regex, cb, channel) {
@@ -28,47 +25,14 @@ bot.match(/fl[oi]p\w*\s*fl[io]p\w*/, (msg) => {
 
 bot.match(/@\s*(\d+):0?(\d+)/, scheduleGame);
 
-function properCaseAll(names, name) {
-  if (name) {
-    const properName = name[0].toUpperCase() + name.substring(1);
-    return names.concat(properName);
-  }
-  return names.concat('');
-}
+bot.match(/.*[cC]ard(.*)/, 
+          (msg, match) => getCardImage(msg, match), '222739491062808576');
 
-function sortFuzzyMatches(a, b) {
-  return a.score > b.score ? -1 : 1;
-}
+bot.match(/.*card search(.*)/,
+          (msg, match) => cardSearch(msg, match), '222739491062808576');
 
-bot.match(/.*[cC]ard(.*)/, (msg, match) => {
-  const possibleNames = match[1].trim().split(' ')
-                              .reduce(properCaseAll, [])
-                              .reduce((ws, w, i, arr) => ws.concat(arr.slice(0,i+1).join(' ')), []);
-  let matched = false;
-  possibleNames.forEach((name) => {
-    if (name in cards) {
-      matched = true;
-      msg.channel.sendMessage('https:' + cards[name].images.medium_stats);
-    }
-  });
-
-  if (!matched) {
-    const closestMatch = possibleNames.reduce((matches, name) => {
-      return matches.concat(fuzzy.filter(name, cardNames).sort(sortFuzzyMatches));
-    }, []).sort(sortFuzzyMatches)[0];
-
-    if (closestMatch && closestMatch.score > 220) {
-      msg.channel.sendMessage('https:' + cards[closestMatch.string].images.medium_stats);
-    }
-  }
-}, '222739491062808576');
-
-bot.match(/.*card search(.*)/, (msg, match) => {
-  //console.log(Object.keys(cards).length);
-  const search = match[1].trim();
-  const names = fuzzy.simpleFilter(search, cardNames);
-  names.forEach((name) => msg.channel.sendMessage(name));
-}, '222739491062808576');
+bot.match(/.*card text(.*)/,
+          (msg, match) => getCardText(msg, match), '222739491062808576');
 
 bot.on('message', msg => {
   console.log(msg.timestamp + ':\t' + msg.author.username + ':' + msg.content);
---
+++ src/paragon.js
@@ -0,0 +1,183 @@
+const request = require('request-promise-native');
+const process = require('process');
+const fuzzy = require('fuzzy');
+
+const apiHeader = {
+  'X-Epic-ApiKey': process.env.PARAGONAPIKEY,
+};
+let cards = {};
+let cardNames = [];
+
+function sortFuzzyMatches(a, b) {
+  return a.score > b.score ? -1 : 1;
+}
+function properCaseAll(names, name) {
+  if (name) {
+    const properName = name[0].toUpperCase() + name.substring(1);
+    return names.concat(properName);
+  }
+  return names.concat('');
+}
+
+class Card {
+  constructor(card) {
+    this._card = card;
+  }
+
+  _parseCardStat(statName, damageType) {
+    switch (statName) {
+      case 'MaxEnergy':
+        return 'Max Mana';
+      case 'AttackRating':
+        console.log(damageType);
+        return damageType === 'Energy' ? 'Energy Damage' : 'Physical Damage';
+      case 'CriticalDamageChance':
+        return 'Crit Chance';
+      case 'CooldownReductionPercentage':
+        return 'Cooldown';
+      case 'MaxHealth':
+        return 'Max Health';
+      case '{attr:enar}':
+      case '{attr:enar}.':
+      case '{attr:enar},':
+      case 'EnergyResistanceRating':
+        return 'Energy Armor';
+      case '{attr:physar}':
+      case '{attr:physar}.':
+      case '{attr:physar},':
+      case 'PhysicalResistanceRating':
+        return 'Physical Armor';
+      case 'EnergyPenetrationRating':
+        return 'Energy Pen';
+      case 'PhysicalPenetrationRating':
+        return 'Physical Pen';
+      case 'HealthRegenRate':
+        return 'Health Regen';
+      case 'ManaRegenRate':
+        return 'Mana Regen';
+      case 'WellRigPlacementTimer':
+        return 'Harvester Placement Time';
+      case '{attr:mp}':
+      case '{attr:mp}.':
+      case '{attr:mp},':
+        return 'Mana';
+      case '{attr:hp}':
+      case '{attr:hp},':
+      case '{attr:hp}.':
+        return 'Health';
+      case '{attr:shld}':
+      case '{attr:shld},':
+      case '{attr:shld}.':
+        return 'Shield';
+      case '{attr:cdr}':
+      case '{attr:cdr}.':
+      case '{attr:cdr},':
+        return 'Cooldown';
+      default:
+        return statName;
+    }
+  }
+
+  _parseCardDescription(description) {
+    return description.split(' ').reduce((description, word) => {
+      console.log('thing', word);
+      let parsedWord = this._parseCardStat(word);
+      return `${description} ${parsedWord}`;
+    }, '');
+  }
+
+  _parseEffectsArray(effects, damageType) {
+    return effects.reduce((effects, effect) => {
+          const descriptor = this._parseCardStat(effect.stat, damageType) ||
+            this._parseCardDescription(effect.description);
+          const value = typeof effect.value === 'number' ? 
+              effect.value.toPrecision(3) : effect.cooldown;
+          console.log('v', effect.description);
+          return `${effects} ${descriptor} ${value || ''}\n`;
+        }, '');
+  }
+
+  getText() {
+    return request({
+      url: `https://developer-paragon.epicgames.com/v1/card/${this._card.id}`,
+      headers: apiHeader,
+    }).then((body) => {
+      const cardDescriptor = JSON.parse(body);
+      const effects = this._parseEffectsArray(cardDescriptor.effects, cardDescriptor.damageType);
+      const maxedEffects = this._parseEffectsArray(cardDescriptor.maxedEffects);
+      const cost = cardDescriptor.cost;
+      const name = cardDescriptor.name;
+      const type = cardDescriptor.slotType;
+
+      if (maxedEffects) {
+        return `*${name}* ${cost}-${type}\n${effects}\nFully Upgraded: ${maxedEffects}`;
+      }
+      return `*${name}* ${cost}-${type}\n${effects}`;
+    });
+  }
+
+  get imageUrl() {
+    return `https:${this._card.images.medium_stats}`;
+  }
+}
+function parseCards(cards) {
+  return cards.reduce((cards, card) => {
+    cards[card.name] = new Card(card);
+    cardNames.push(card.name);
+    return cards;
+  }, {});
+}
+
+request({
+  url: 'https://developer-paragon.epicgames.com/v1/cards',
+  headers: apiHeader,
+}).then((body) => {
+  cards = parseCards(JSON.parse(body)); 
+}).catch((err) =>  console.log(err));
+
+function getClosestCardToMatch(match) {
+  const possibleNames = match[1].trim().split(' ')
+                              .reduce(properCaseAll, [])
+                              .reduce((ws, w, i, arr) => ws.concat(arr.slice(0,i+1).join(' ')), []);
+  possibleNames.forEach((name) => {
+    if (name in cards) {
+      return cards[name];
+    }
+  });
+
+  const closestMatch = possibleNames.reduce((matches, name) => {
+    return matches.concat(fuzzy.filter(name, cardNames).sort(sortFuzzyMatches));
+  }, []).sort(sortFuzzyMatches)[0];
+
+  if (closestMatch && closestMatch.score > 220) {
+    return cards[closestMatch.string];
+  }
+
+  return null;
+}
+
+function cardSearch(msg, match) {
+  const search = match[1].trim();
+  const names = fuzzy.simpleFilter(search, cardNames);
+  names.forEach((name) => msg.channel.sendMessage(name));
+}
+
+function getCardImage(msg, match) {
+  const card = getClosestCardToMatch(match);
+  if (card) {
+    msg.channel.sendMessage(card.imageUrl);
+  }
+}
+
+function getCardText(msg, match) {
+  const card = getClosestCardToMatch(match);
+  if (card) {
+    card.getText().then((text) => msg.channel.sendMessage(text));
+  }
+}
+
+module.exports = {
+  cardSearch,
+  getCardImage,
+  getCardText,
+};
